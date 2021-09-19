ac596e125e7a565d14b82294c16bb6a77f6fd14d
donglaiw <donglai@holygpu7c1706.rc.fas.harvard.edu>
2021-09-19 19:36:54 -0400

    update seg_track IoU param

--- scripts/test_seg.py
+++ scripts/test_seg.py
@@ -3,12 +3,12 @@ from emu.seg import *
 from emu.io import writeH5
 import numpy as np
 
-def test_seg_track(seg):
+def test_seg_track(seg, iou_thres=0.2):
     from emu.seg import predToSeg2d,seg2dToIoU
     seg3d_naive = predToSeg2d(seg, [-1])
     del seg
-    iou = seg2dToIoU(seg3d_naive)
-    seg3d = seg2dTo3d(seg3d_naive, iou, 0.4)
+    iou = seg2dToIoU(seg3d_naive, iou_thres)
+    seg3d = seg2dTo3d(seg3d_naive, iou)
     return seg3d
 
 
