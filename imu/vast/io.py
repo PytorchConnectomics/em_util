c85d8a423d19109b9911df49dd1137f85c088a06
donglaiw <donglai@boslogin03.rc.fas.harvard.edu>
2022-01-23 13:05:26 -0500

    combine mipmap generation

--- imu/vast/io.py
+++ imu/vast/io.py
@@ -3,16 +3,23 @@ from scipy.ndimage import zoom
 from imu.io import mkdir
 from imageio import imsave
 
-def createMipImages(getInputImage, getOutputName, zran, level_start=0, level_num=3, resize_order=1):
+def createMipImages(getInputImage, getOutputName, zran, level_ran=range(3), resize_order=1):
+    # need helper function to get image slice from 3D volume or image namges
     # getInputImage(z): image at slice z 
     # getOutputName(m, z): filename at mip m and slice z 
-    for m in range(level_start, level_start+level_num):
+    for m in level_ran:
         output_name = getOutputName(m, 0)
         output_folder = output_name[:output_name.rfind('/')]
         mkdir(output_folder)
 
     for z in zran:
         im = getInputImage(z)
-        for m in range(level_start, level_start+level_num):
-            imsave(getOutputName(m, z), im)
+        level_ran = [x for x in level_ran if x>0]
+        # downsample until first mip level
+        for i in range(level_ran[0]-1):
             im = zoom(im, 0.5, order=resize_order)
+
+        for m in level_ran:
+            imsave(getOutputName(m, z), im)
+            if m != level_ran[-1]:
+                im = zoom(im, 0.5, order=resize_order)
