08737993708f9f276b145da0d93add52fc47e55e
donglaiw <weidf@l001.cm.cluster>
2024-11-11 22:05:29 -0500

    update vast meta

--- em_util/vast/meta.py
+++ em_util/vast/meta.py
@@ -14,18 +14,17 @@ def read_vast_seg(fn):
     a = open(fn).readlines()
     # remove comments
     st_id = 0
-    while a[st_id][0] in ["%", "\\", '\n']:
+    while a[st_id][0] in ["%", '\n']:
         st_id += 1
     
-    st_id -= 1
     # remove segment name
-    out = np.zeros((len(a) - st_id - 1, 24), dtype=int)
-    name = [None] * (len(a) - st_id - 1)
-    for i in range(st_id + 1, len(a)):
-        out[i - st_id - 1] = np.array(
+    out = np.zeros((len(a) - st_id, 24), dtype=int)
+    name = [None] * (len(a) - st_id)
+    for i in range(st_id, len(a)):
+        out[i - st_id] = np.array(
             [int(x) for x in a[i][: a[i].find('"')].split(" ") if len(x) > 0]
         )
-        name[i - st_id - 1] = a[i][a[i].find('"') + 1 : a[i].rfind('"')]
+        name[i - st_id] = a[i][a[i].find('"') + 1 : a[i].rfind('"')]
     return out, name
 
 
