c28a1cbae7973749d838918e8bf80192cfdf3db1
donglaiw <donglai@boslogin08.rc.fas.harvard.edu>
2025-01-22 23:20:50 -0500

    update read_h5

--- em_util/io/io.py
+++ em_util/io/io.py
@@ -247,7 +247,7 @@ def read_h5(filename, dataset=None, chunk_id=0, chunk_num=1):
 
     out = [None] * len(dataset)
     for di, d in enumerate(dataset):
-        out[di] = read_vol_chunk(fid[d], chunk_id, chunk_num)
+        out[di] = np.array(fid[d])
 
     return out[0] if len(out) == 1 else out
 
