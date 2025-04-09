bcd391b06e69190bfac776d5105d7bdca62a93ac
donglaiw <donglai@boslogin06.rc.fas.harvard.edu>
2025-04-09 00:42:28 -0400

    add read nd2

--- em_util/io/io.py
+++ em_util/io/io.py
@@ -157,15 +157,13 @@ def read_vol_bbox(filename, bbox, dataset=None, ratio=1, resize_order=0):
     result = resize_image(result, ratio, resize_order)
     return result
 
-def read_vol(filename, dataset=None, chunk_id=0, chunk_num=1):   
+def read_vol(filename, dataset=None):
     """
     Read data from various file formats.
 
     Args:
         filename (str): The path to the file.
         dataset (str, optional): The name of the dataset within the file. Defaults to None.
-        chunk_id (int, optional): The ID of the chunk to read. Defaults to 0.
-        chunk_num (int, optional): The total number of chunks. Defaults to 1.
 
     Returns:
         numpy.ndarray: The read data.
@@ -180,9 +178,11 @@ def read_vol(filename, dataset=None, chunk_id=0, chunk_num=1):
     elif filename[-3:] in ["tif", "iff"]:
         out = read_image(filename, data_type="nd")
     elif filename[-2:] == "h5":
-        out = read_h5(filename, dataset, chunk_id, chunk_num)
+        out = read_h5(filename, dataset)
     elif filename[-3:] == "zip":
-        out = read_zarr(filename, dataset, chunk_id, chunk_num)
+        out = read_zarr(filename, dataset)
+    elif filename[-3:] == "nd2":
+        out = read_nd2(filename, dataset)
     elif len(filename) > 11 and (filename[:11] == "precomputed" or filename[:3] == "gs:"):
         import cloudvolume
         if filename[:11] != "precomputed":
@@ -198,15 +198,41 @@ def read_vol(filename, dataset=None, chunk_id=0, chunk_num=1):
         raise f"Can't read the file type of {filename}"
     return out
 
-def read_zarr(filename, dataset=None, chunk_id=0, chunk_num=1):
+def read_nd2(filename, dataset=None):
+    """
+    Read data from a nd2 file.
+
+    Args:
+        filename (str): The path to the nd2 file.
+        dataset (int, optional): The frame id of the dataset within the file. Defaults to -1 for all frames.
+
+    Returns:
+        numpy.ndarray: The read data.
+
+    Raises:
+        Exception: If the dataset is not found or if there is an error reading the file.
+    """
+    from aicsimageio import AICSImage
+    img = AICSImage(filename)
+    frames = img.data
+    if isinstance(dataset, int):
+        assert(dataset < len(frames), f'{dataset}-th frame is out of bound. #frames={len(frames)}')
+        data = np.array(frames[dataset])
+    else:
+        if dataset is None:
+            dataset = range(len(frames))
+        assert(np.max(dataset) < len(frames), f'{np.max(dataset)}-th frame is out of bound. #frames={len(frames)}')
+        data = [np.array(frames[x]) for x in dataset]
+    
+    return data
+ 
+def read_zarr(filename, dataset=None):
     """
     Read data from a Zarr file.
 
     Args:
         filename (str): The path to the Zarr file.
         dataset (str, optional): The name of the dataset within the file. Defaults to None.
-        chunk_id (int, optional): The ID of the chunk to read. Defaults to 0.
-        chunk_num (int, optional): The total number of chunks. Defaults to 1.
 
     Returns:
         numpy.ndarray: The read data.
@@ -222,7 +248,7 @@ def read_zarr(filename, dataset=None, chunk_id=0, chunk_num=1):
         if "," in dataset:
             dataset = dataset[: dataset.find(",")]
     
-    return read_vol_chunk(fid[dataset], chunk_id, chunk_num)
+    return read_vol(fid[dataset])
     
 def read_h5(filename, dataset=None):
     """
